809b1bd7ef88fa2abfb5d3f939d44c2ce1720a34
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-11-06 01:52:08 -0300

    - Basic Data types

---
+++ TP-Clojure-Examples/src/dataTypes.clj
@@ -0,0 +1,38 @@
+(ns .dataTypes)
+
+;;Set
+  (println (set '(1 2 3 4)))
+
+;; Distinct elements
+  (println (set '(1 1 2 3 2 4 5 5)))
+
+;; Distinc elements
+  (println (set [1 1 2 3 2 4 5 5]))
+
+
+;;
+  (println (set [1 2 3 4 5]))
+
+;;
+  (println (set "abcdef"))
+
+;;
+  (println (set '("a" "b" "c" "d")))
+
+;;
+  (println (set {:uno 1 :dos 2 :tres 3}))
+
+;;
+  (println (set nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Listas
+
+  (println (list 1 2 3 4))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vectores
+
+  (println (vector 1 2 3 4))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
