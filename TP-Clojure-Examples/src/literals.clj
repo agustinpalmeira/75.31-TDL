0687e371dce681e15986a60e2b3cbe617bb3af17
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-11-06 01:21:38 -0300

    - BigInt and BigDecimal literals examples

---
+++ TP-Clojure-Examples/src/literals.clj
@@ -0,0 +1,8 @@
+(ns .literals)
+  (println (== 1 1.0 1M))
+  (println (/ 4 2))
+  (println (/ 2.0 3))
+  (println (map #(Math/abs %) (range -3 3)))
+  (println (class 92399821837N))
+  (println (class 1.45412123M))
+
