92fe920ff9ccf7d776673d81979edfd60d8fa9de
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-12-03 22:16:05 -0300

    - examples added

--- TP-Clojure-Examples/src/basicOperations.clj
+++ TP-Clojure-Examples/src/basicOperations.clj
@@ -7,6 +7,23 @@
 (println (is-small? 50))
 (println (is-small? 500))
 
+;; Salida:
+;; yes
+;; no
 
+(println (if(even? 6) "even" "odd"))
 
+(even? 2)
+(even? 1)
 
+(defn if-let-demo [arg]
+  (if-let [x arg]
+    "then"
+    "else"))
+
+(println (if-let-demo 1)) ; anything except nil/false
+;;=> "then"
+(println (if-let-demo nil))
+;;=> "else"
+(println (if-let-demo false))
+;;=> "else"
