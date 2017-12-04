299ee68699ce94e91902228ae106a04480413aa8
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-12-04 11:04:53 -0300

    - Examples with basic operations added

--- TP-Clojure-Examples/src/basicOperations.clj
+++ TP-Clojure-Examples/src/basicOperations.clj
@@ -27,3 +27,29 @@
 ;;=> "else"
 (println (if-let-demo false))
 ;;=> "else"
+
+(if (even? 5)
+  (do (println "even") "not odd")
+  (do (println "odd") "not even"))
+
+(when true (println "true"))
+
+(defn filter-evens [coll]
+  (if-let [evens (seq (filter even? coll))]
+    (println (str "Evens are:" evens))
+    (println "No evens.")))
+
+(let [x 5] ;;binds 5 to x
+  (cond
+    (< x 2)  (println "x is less than 2")
+    (< x 10) (println "x is less than 10")
+
+    :else (println "x is greater than or equal to 10")))
+
+(defn value [x]
+  (case x
+    5 "x is 5"
+    10 "x is 10"
+    "x isn't 5 or 10"))
+(value 11) ;;"x isn't 5 or 10"
+(value 10);;"x is 10
